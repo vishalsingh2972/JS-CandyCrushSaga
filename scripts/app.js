11123e4e365d12708b33866a1b40c79d2cee4fb9
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-07-24 15:47:59 +0530

    after drop now new candies appear feature added

--- scripts/app.js
+++ scripts/app.js
@@ -107,14 +107,16 @@ document.addEventListener('DOMContentLoaded',() => {
       if(squares[i + width].style.backgroundColor === ''){
         squares[i + width].style.backgroundColor = squares[i].style.backgroundColor;
         squares[i].style.backgroundColor = '';
+        const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+        const isFirstRow = firstRow.includes(i);
+        if(isFirstRow && squares[i].style.backgroundColor === ''){
+        //if(firstRow.includes(i) && squares[i].style.backgroundColor === ''){ //direct attack also possible lol
+          let randomColor = Math.floor(Math.random() * candyColors.length);
+          squares[i].style.backgroundColor = candyColors[randomColor];
+        }
       }
     }
-     
   }
-
-
-
-
 
 //Checking for matches
   //check for row of three
